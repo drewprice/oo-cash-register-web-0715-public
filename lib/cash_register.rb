1440912d494c14ab2349d5470acbd4c7724363c0
Drew Price <drewprice@users.noreply.github.com>
2015-08-05 18:13:56 -0400

    Add solution

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+class CashRegister
+  attr_reader :discount, :items
+  attr_accessor :total
+
+  def initialize(discount = 0)
+    @total    = 0
+    @discount = discount
+    @items    = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    quantity.times { @items << item }
+
+    @total += (price * quantity)
+  end
+
+  def apply_discount
+    return 'There is no discount to apply.' if @discount == 0
+
+    @total *= discount_formatted
+
+    "After the discount, the total comes to $#{total_display}."
+  end
+
+  private
+
+  def total_display
+    @total.round == @total ? @total.round : @total
+  end
+
+  def discount_formatted
+    1 - (20.fdiv(100))
+  end
+end
